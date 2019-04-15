5e68f586ca346a0e5a2f78d8e9ba53c18ce9a042
pj-jack <yuchengrun@pujiangkeji.net>
2019-04-15 18:54:01 +0800

    frist

---
+++ library-base/src/main/java/jack/library_base/IModuleInit.java
@@ -0,0 +1,18 @@
+package jack.library_base;
+
+import android.app.Application;
+
+/**
+ * created by Jack
+ * email:yucrun@163.com
+ * date:19-4-15
+ * describe:
+ */
+public interface IModuleInit {
+    //初始化优先的
+    boolean onInitAhead(Application application);
+
+    //初始化靠后的
+    boolean onInitLow(Application application);
+
+}
---
+++ library-base/src/main/java/jack/library_base/ModuleLifecycleConfig.java
@@ -0,0 +1,11 @@
+package jack.library_base;
+
+/**
+ * created by Jack
+ * email:yucrun@163.com
+ * date:19-4-15
+ * describe:
+ */
+public class ModuleLifecycleConfig {
+
+}
